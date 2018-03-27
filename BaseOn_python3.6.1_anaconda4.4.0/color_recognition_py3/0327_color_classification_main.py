a7585a2b488cac70b03ff11ccf970975e77c2863
CharlieLi-CHINA <1198194789@qq.com>
2018-03-27 20:51:06 +0800

    基于Python3的车辆颜色识别

---
+++ BaseOn_python3.6.1_anaconda4.4.0/color_recognition_py3/0327_color_classification_main.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------
+"""
+fuction:基于knn的颜色识别
+@author: liqilv
+"""
+from __future__ import division
+import numpy as np
+import cv2,time,sys,os
+import knn_classifier
+from PIL import Image  
+from skimage import io
+import color_histogram_feature_extraction
+ 
+#checking whether the training data is ready
+PATH='./training.data'
+
+if os.path.isfile(PATH) and os.access(PATH, os.R_OK):  
+    #如果path是一个存在的文件，返回True。否则返回False
+    # os.R_OK来测试PATH是否可读
+    print ("training data is ready, classifier is loading...")
+else:
+    print ("training data is being created...")
+    open('training.data','w')
+    color_histogram_feature_extraction.training()
+    print ("training data is ready, classifier is loading...")
+
+def cut_lower_half(img):
+    #剪切图像
+    x,y = img.size
+    box = [1,y/2,x,y]
+    img2 = img.crop(box)
+    img2.save("linshi.jpg")
+
+def file_name(file_dir):  
+    pic_name= []
+    for root, dirs, files in os.walk(file_dir):  
+    #print(files) #当前路径下所有非目录子文件
+        pic_name.append(files)
+    return pic_name   
+
+#用于批量处理
+pic_name= file_name("./test_picture")#待识别图像文件名字的读取
+for i in range(0,len(pic_name)):
+    picture_name= pic_name[i]
+    
+#批量读入图像并存在coll
+#print pic_name
+coll = io.ImageCollection("./test_picture/*.jpg")
+
+print ('the mount of the picture is :',len(coll))
+
+mount_all = len(coll)
+mount = [0,0,0,0,0,0,0,0,0,0]
+
+for i in range(0,len(coll)):
+    
+    io.imsave('test1.jpg',coll[i])
+    io.imsave('test2.jpg',coll[i])
+    img1 = Image.open("test1.jpg")
+    cut_lower_half(img1)
+    frame = cv2.imread("linshi.jpg")
+    pre_class = cv2.imread("test1.jpg")
+    prediction = "n.a."
+    frame2 =  cv2.imread("test2.jpg")
+    color_histogram_feature_extraction.color_histogram_of_test_image(frame)
+    
+    prediction = knn_classifier.main("training.data", "test.data")
+    cv2.putText(frame2,  prediction, (15, 45 ), cv2.FONT_HERSHEY_PLAIN, 3, (255,255,255),5)
+    cv2.imshow('pre color',pre_class)
+    #cv2.waitKey(0)&0xFF 
+    cv2.imshow('color classifier',frame2)
+    
+    #用于统计正确率
+    if prediction == "black":
+        mount[0] = mount[0] + 1
+    elif prediction == "gray":
+        mount[1] = mount[1] + 1
+    elif prediction == "white":
+        mount[2] = mount[2] + 1
+    elif prediction == "red":
+        mount[3] = mount[3] + 1
+    elif prediction == "yellow":
+        mount[4] = mount[4] + 1
+    elif prediction == "blue":
+        mount[5] = mount[5] + 1
+    elif prediction == "green":
+        mount[6] = mount[6] + 1
+    elif prediction == "purple":
+        mount[7] = mount[7] + 1
+    elif prediction == "pink":
+        mount[8] = mount[8] + 1
+    elif prediction == "brown":
+        mount[9] = mount[9] + 1
+    #statistics_most_ture(prediction)
+    os.remove('test1.jpg')
+    os.remove('test2.jpg')
+    
+    #识别结果写进去txt
+    fp = open('./result/test.txt','a+')
+    fp.write(picture_name[i] + ' ' + prediction + '\n')
+    fp.close()
+    cv2.waitKey(0)&0xFF
+
+#修改结果名字txt
+result_txt_name = time.strftime('%Y%m%d_%H%M%S',time.localtime(time.time()))
+result_name = result_txt_name + '.txt'    
+
+os.rename('./result/test.txt', './result/'+ result_name)
+mount_ture = max(mount)
+ture_rate = float(mount_ture / mount_all)
+str(ture_rate)
+# print ("准确率：", ture_rate)
+cv2.destroyAllWindows() 
---
+++ BaseOn_python3.6.1_anaconda4.4.0/color_recognition_py3/readme.md
@@ -0,0 +1,67 @@
+# color_recognition 说明
+
+编程参考
+> [https://github.com/ahmetozlu/color_recognition](https://github.com/ahmetozlu/color_recognition)
+
+## 环境
+
+Python2.7,用到的库包括: numpy,cv2,time,sys,os,PIL(主要用到Image),skimage,matplotlib,scipy,csv,random,math,operator
+
+
+## 文件说明
+
+### ./result    存放识别结果的txt文档
+###  ./test_picture    测试图像文件夹
+###  ./training_dateset    训练样本文件夹
+###  test.data    最新一次的测试获得的图像rgb直方图值
+###  training.data    颜色分类范围文件
+###  knn_classifier.py knn分类
+###  color_histogram_feature_extraction.py 特征提取程序
+###  0326i_color_classification_main.py 主程序
+
+
+## 运行
+
+### 总的思路
+
+1. 特征提取：通过训练图像中的rgb颜色直方图进行特征提取
+2. 训练KNN分类器：通过RGB颜色直方图 的值来训练KNN分类器
+3. 通过训练KNN来分类：对图像进行特征提取，然后通过训练KNN分类器来分类中值颜色
+
+### 具体执行
+
+- 首次运行需要执行 knn_classifier.py
+以及color_histogram_feature_extraction.py文件
+即：
+> 调用feature_extraction.py以通过特征提取创建训练数据
+
+> 调用knn_classifier.py进行分类
+
+- 然后执行主程序
+
+- 从 ./test_picture加载图像，执行for循环时，
+每次读入的图像先保存为test.jpg(test后面或许有后缀，如test1.jpg），
+经过裁剪后保存为linshi.jpg，经过knn识别后（就是上面加载的那两个.py文件）输出预测结果，并写在图像上输出，然后下一个。
+
+
+### 额外
+
+“knn_classifier.py”的解释
+
+这个程序提供了这些主要计算;
+
+- 获取训练数据
+- 获取测试图像功能
+- 计算欧氏距离
+- 获得k近邻点
+- 颜色预测
+- 返回预测是对还是错
+
+
+
+## 理论
+
+在这里，颜色通过使用KNN机器学习分类器算法进行分类，这个分类器通过训练图像的RGB颜色直方图的值来进行分类，其工作原理框架如下图：
+![](https://user-images.githubusercontent.com/22610163/35335133-a9632c70-0125-11e8-9204-0b4bfd0702a7.png)
+
+
